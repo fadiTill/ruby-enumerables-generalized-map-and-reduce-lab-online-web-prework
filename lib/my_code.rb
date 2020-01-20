0fab77d0231497d069e3d7a6c15b1b1d0b705e82
fadimata tillman <fadi.tillman@gmail.com>
2020-01-20 00:05:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,5 @@
 # Your Code Here
+def fadi_map [ num1,num2,mun3,num4;multiply]
+  it "returns an array with all values made negative" do
+    expect(map([1, 2, 3, -9]){|n| n * -1}).to eq([-1, -2, -3, 9])
+  end
